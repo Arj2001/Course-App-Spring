7222fc69b116ae698beae328c5bda3657ec7e084
Arj2001 <75932871+Arj2001@users.noreply.github.com>
2022-10-17 11:28:57 +0530

    Initlialized

---
+++ src/main/java/com/nestdigital/CourseApp/controller/CourseController.java
@@ -0,0 +1,30 @@
+package com.nestdigital.CourseApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @GetMapping("/")
+    public String home(){
+        return "Welcome to home page";
+    }
+
+    @PostMapping("/read")
+    public String readValue(){
+        return "This is a post method";
+    }
+
+    @GetMapping("/viewall")
+    public List viewAll(){
+        List<String> myList = new ArrayList<>();
+        myList.add("{'name':'Varun','age':22}");
+        return myList;
+    }
+
+}
